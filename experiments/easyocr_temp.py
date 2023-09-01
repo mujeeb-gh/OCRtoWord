1bf51fbbeb54119d25f6f0082cc046e2701d0762
mujeeb-gh <olamidebalogun174@gmail.com>
2023-09-01 10:27:53 +0100

    experiments folder

---
+++ experiments/easyocr_temp.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import easyocr
+import pyperclip
+
+IMAGE_PATH = "Images/text_1.png"
+
+reader = easyocr.Reader(['en'], gpu= False)
+result = reader.readtext(IMAGE_PATH, detail= 0)
+print(result)
+pyperclip.copy(str(result))
