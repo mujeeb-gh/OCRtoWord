458d05debc368f52fcd7ab317a2072a1d89784ee
mujeeb-gh <olamidebalogun174@gmail.com>
2023-09-13 15:53:56 +0100

    endpoint update

--- experiments/testimage_reader2.py
+++ experiments/testimage_reader2.py
@@ -1,62 +1,10 @@
-import cv2
+from PIL import Image
+import base64, cv2
 
-# Load the pre-trained EAST text detection model
-model_path = 'frozen_east_text_detection.pb'
-net = cv2.dnn.readNet(model_path)
+# with open("experiments/Images/text_1.png", "rb") as f:
+#     image = Image.open(f)
 
-# Load the image
-image_path = 'pictures/image text 6.jpg'
-image = cv2.imread(image_path)
-height, width = image.shape[:2]
+image = cv2.imread("experiments/Images/text_1.png")
 
-# Resize the image for EAST model (should be multiples of 32)
-new_width = (width // 32) * 32
-new_height = (height // 32) * 32
-resized_image = cv2.resize(image, (new_width, new_height))
-
-# Define the confidence threshold and non-maximum suppression threshold
-conf_threshold = 0.5
-nms_threshold = 0.4
-
-# Get the output layer names
-output_layer_names = net.getUnconnectedOutLayersNames()
-
-# Prepare the input blob for the EAST model
-blob = cv2.dnn.blobFromImage(resized_image, scalefactor=1.0, size=(new_width, new_height), mean=(123.68, 116.78, 103.94), swapRB=True, crop=False)
-
-# Set the input blob for the network
-net.setInput(blob)
-
-# Forward pass through the network
-output = net.forward(output_layer_names)
-
-# Post-process the output
-boxes = []
-confidences = []
-for layer_output in output:
-    for detection in layer_output[0, 0]:
-        score = detection[2]
-        if score > conf_threshold:
-            left = int(detection[3] * width)
-            top = int(detection[4] * height)
-            right = int(detection[5] * width)
-            bottom = int(detection[6] * height)
-            width_box = right - left
-            height_box = bottom - top
-            # Filter out small boxes
-            if width_box > 10 and height_box > 10:
-                boxes.append((left, top, width_box, height_box))
-                confidences.append(float(score))
-
-# Apply non-maximum suppression
-indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
-
-# Draw the bounding boxes on the image
-for i in indices:
-    left, top, width_box, height_box = boxes[i[0]]
-    cv2.rectangle(image, (left, top), (left + width_box, top + height_box), (0, 255, 0), 2)
-
-# Display the image with bounding boxes
-cv2.imshow('Text Detection', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+encoded_image = base64.b64encode(image)
+print(type(encoded_image))
--- ocr_backend/ocr_app/views.py
+++ ocr_backend/ocr_app/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, HttpResponse
+from django.http import FileResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import *
 from PIL import Image, ImageFilter
+from docx import Document
 import base64, pytesseract
 import numpy as np
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 # Create your views here.
+# Image upload endpoint
 @api_view(['POST'])
 def upload_image(request):
   if request.method == 'POST':
@@ -26,6 +29,7 @@ def upload_image(request):
     else:
       return Response({'error': 'No image data provided.'}, status=status.HTTP_400_BAD_REQUEST)
     
+# Text extraction endpoint
 @api_view(['POST'])
 def extract_text(request):
   if request.method == 'POST':
@@ -34,6 +38,7 @@ def extract_text(request):
         image = ImageModel.objects.get(pk=image_id)
         if image.file.name.lower().endswith(('jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff')):
           with open(image.file.path, 'rb') as image_file:
+            global extracted_text
             extracted_text = pytesseract.image_to_string(Image.open(image_file))
           extracted_text = extracted_text.replace('\n', ' ')
         else:
@@ -44,18 +49,10 @@ def extract_text(request):
   else:
     return Response({'error': 'No image identifier provided.'}, status=status.HTTP_400_BAD_REQUEST)   
       
-    
-
-# @api_view(['POST'])
-# def extract_text(request):
-#   if request.method == 'POST':
-#     image_file = request.data.get('image')
-#     if image_file:
-#       try:
-#         extracted_text = pytesseract.image_to_string(image= Image.open(image_file))
-#         extracted_text = extracted_text.replace('\n', ' ')
-#         return Response({'text': extracted_text}, status= status.HTTP_200_OK)
-#       except Exception as e:
-#         return Response({'error': f'Text extraction error: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-#     else:
-#       return Response({'error': 'No image data provided.'}, status=status.HTTP_400_BAD_REQUEST)
+# word document generation endpoint
+@api_view(['POST'])
+def generate_document(request):
+  if request.method == 'POST':
+    if extracted_text:
+      document = Document()
+      document.add_heading('Extracted Text', level)
